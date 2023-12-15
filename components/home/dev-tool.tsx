d8ba75317dc7fdc435107c59aebc8e7e47a443ad
pirangy <pirangy.ariel@gmail.com>
2023-12-15 07:53:35 -0300

    Config: adjusted footer, excluded redundant components

--- components/home/dev-tool.tsx
+++
@@ -1,7 +0,0 @@
-import NextJS from "@/components/shared/icons/nextjs";
-import ReactJS from "@/components/shared/icons/react";
-import Angular from "@/components/shared/icons/angular";
-import TypeScript from "@/components/shared/icons/typescript";
-import Java from "@/components/shared/icons/java";
-import SpringBoot from "@/components/shared/icons/springboot";
-
