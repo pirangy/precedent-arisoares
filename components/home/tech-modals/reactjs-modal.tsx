899b22d044b26ea95c89d15da5b6c8bf4f3da400
pirangy <pirangy.ariel@gmail.com>
2023-12-17 17:43:39 -0300

    Config: tool-modals Icons OK, description TBD

--- components/home/tech-modals/reactjs-modal.tsx
+++ components/home/tech-modals/reactjs-modal.tsx
@@ -7,6 +7,7 @@ import {
   useMemo,
 } from "react";
 import Image from "next/image";
+import ReactJS from "@/components/shared/icons/react";
 
 const ReactJSModal = ({
   showReactJSModal,
@@ -19,13 +20,9 @@ const ReactJSModal = ({
     <Modal showModal={showReactJSModal} setShowModal={setShowReactJSModal}>
       <div className="w-full overflow-hidden md:max-w-md md:rounded-2xl md:border md:border-gray-100 md:shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 bg-white px-4 py-6 pt-8 text-center md:px-16">
-          <a href="https://precedent.dev">
-            <Image
-              src="/logo.png"
-              alt="Precedent Logo"
+          <a href="https://react.dev/">
+            <ReactJS
               className="h-10 w-10 rounded-full"
-              width={20}
-              height={20}
             />
           </a>
           <h3 className="font-display text-2xl font-bold">React.js</h3>
--- components/home/tech-modals/spring-modal.tsx
+++ components/home/tech-modals/spring-modal.tsx
@@ -7,6 +7,7 @@ import {
   useMemo,
 } from "react";
 import Image from "next/image";
+import SpringBoot from "@/components/shared/icons/springboot";
 
 const SpringModal = ({
   showSpringModal,
@@ -20,12 +21,8 @@ const SpringModal = ({
       <div className="w-full overflow-hidden md:max-w-md md:rounded-2xl md:border md:border-gray-100 md:shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 bg-white px-4 py-6 pt-8 text-center md:px-16">
           <a href="https://precedent.dev">
-            <Image
-              src="/logo.png"
-              alt="Precedent Logo"
+            <SpringBoot
               className="h-10 w-10 rounded-full"
-              width={20}
-              height={20}
             />
           </a>
           <h3 className="font-display text-2xl font-bold">Spring</h3>
